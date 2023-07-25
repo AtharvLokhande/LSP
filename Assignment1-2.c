c8b99a696f70f7547ed0004a9dbc91ab5a3fcd91
AtharvLokhande <121097976+AtharvLokhande@users.noreply.github.com>
2023-07-25 15:59:59 +0530

    Add files via upload

---
+++ Assignment1-2.c
@@ -0,0 +1,60 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Problem Statement :
+//          write a Program which accept file name and mode from user and open that file in specified mode.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+//          Author        :     Atharv Lokhande
+//          Application   :     Used to open the any file in specified mode according to user.
+//          Input         :     1.File Name Which we have to open.
+//                              2.File mode in which we have open file.
+//          Output        :     Open the Given file in given mode
+//          Date          :     25/07/2023
+/////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    int fd = 0;
+    int Mode = 0;
+
+    
+    if(argc != 3)
+    {
+        printf("Invalid number of arguments\n");
+        return -1;
+    }
+
+    if(strcmp(argv[2],"Read") == 0)
+    {
+        Mode = O_RDONLY;
+    }
+    else if(strcmp(argv[2],"Write") == 0)
+    {
+        Mode = O_WRONLY;
+    }
+    else
+    {
+        Mode = O_RDONLY;
+    }
+
+    fd = open(argv[1], Mode);
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return -1;
+    }
+    else
+    {
+        printf("File is succesfully opened with fd : %d\n",fd);
+    }
+    close(fd);
+
+    return 0;
+}
